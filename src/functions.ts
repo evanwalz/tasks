998051d80e608854306d627eb2cf53abe2a1bf65
evanwalz <evanwalz@udel.edu>
2025-09-09 19:26:48 -0400

    Completed add3 function

--- src/functions.ts
+++ src/functions.ts
@@ -16,7 +16,7 @@ export function add3(first: number, second: number, third: number): number {
     if (first > 0) sum += first;
     if (second > 0) sum += second;
     if (third > 0) sum += third;
-    return 0;
+    return sum;
 }
 
 /**
