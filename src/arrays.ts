08e67dcebd557c486fab25ca1d9b2a2ce6d4dde8
evanwalz <evanwalz@udel.edu>
2025-09-14 17:23:35 -0400

    Completed makeMath

--- src/arrays.ts
+++ src/arrays.ts
@@ -95,7 +95,15 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const sum = addends.reduce(
+        (currentTotal: number, num: number) => currentTotal + num,
+        0,
+    );
+    const numbersStr = addends.join("+");
+    return `${sum}=${numbersStr}`;
 }
 
 /**
