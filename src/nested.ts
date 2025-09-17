d62e02373ae3573a47f83cb4bccbe855093e02fc
evanwalz <evanwalz@udel.edu>
2025-09-17 13:32:29 -0400

    Completed removeQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -48,7 +48,10 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    return [];
+    const removedQuestion = questions.filter(
+        (quest: Question): boolean => quest.id != id,
+    );
+    return removedQuestion;
 }
 
 /***
