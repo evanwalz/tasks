8fd7ed67c8bfa56313815e39f8dc1de28dd28bdc
evanwalz <evanwalz@udel.edu>
2025-09-24 14:12:31 -0400

    fixed CycleHoliday to match assignment

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,43 +1,30 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-type Holiday =
-    | "NewYear"
-    | "Easter"
-    | "Independence"
-    | "Halloween"
-    | "Christmas";
-
-const emoji: Record<Holiday, string> = {
-    NewYear: "🎉",
-    Easter: "🐣",
-    Independence: "🇺🇸",
-    Halloween: "🎃",
-    Christmas: "🎄",
-};
+type Holiday = "🎉" | "🐣" | "🇺🇸" | "🎃" | "🎄";
 
 const alphabetically: Record<Holiday, Holiday> = {
-    Christmas: "Easter",
-    Easter: "Halloween",
-    Halloween: "Independence",
-    Independence: "NewYear",
-    NewYear: "Christmas",
+    "🎄": "🐣",
+    "🐣": "🎃",
+    "🎃": "🇺🇸",
+    "🇺🇸": "🎉",
+    "🎉": "🎄",
 };
 
 const chronologically: Record<Holiday, Holiday> = {
-    NewYear: "Easter",
-    Easter: "Independence",
-    Independence: "Halloween",
-    Halloween: "Christmas",
-    Christmas: "NewYear",
+    "🎉": "🐣",
+    "🐣": "🇺🇸",
+    "🇺🇸": "🎃",
+    "🎃": "🎄",
+    "🎄": "🎉",
 };
 
 export function CycleHoliday(): React.JSX.Element {
-    const [holiday, setHoliday] = useState<Holiday>("NewYear");
+    const [holiday, setHoliday] = useState<Holiday>("🎉");
 
     return (
         <div>
-            <div>Holiday: {emoji[holiday]}</div>
+            <div>Holiday: {holiday}</div>
 
             <Button
                 onClick={() => {
