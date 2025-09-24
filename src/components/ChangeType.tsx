b6f4650eb9582361abc567ed9d29edee642ea93e
evanwalz <evanwalz@udel.edu>
2025-09-24 12:54:11 -0400

    Completed ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,21 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    return (
+        <span>
+            <Button
+                onClick={() => {
+                    if (type === "short_answer_question") {
+                        setType("multiple_choice_question");
+                    } else {
+                        setType("short_answer_question");
+                    }
+                }}
+            >
+                {" "}
+                Change Type{" "}
+            </Button>
+        </span>
+    );
 }
